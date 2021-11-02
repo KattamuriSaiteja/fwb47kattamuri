54b0d507185ddd03f53aa908d8a8ac491a5d0631
Kattamuri Saiteja <34226707+Kattamur@users.noreply.github.com>
2021-11-01 23:59:25 -0500

    added addmod pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var earRouter = require('./routes/earphones');
+var modsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/earphones', earRouter);
+app.use('/addmods', modsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
