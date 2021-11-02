92a504e2081855b1f54f987ea630521f4f95a044
Kattamuri Saiteja <34226707+Kattamur@users.noreply.github.com>
2021-11-02 00:22:49 -0500

    Added bonus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var earRouter = require('./routes/earphones');
 var modsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/earphones', earRouter);
 app.use('/addmods', modsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
